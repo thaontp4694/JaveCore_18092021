1980e91267ab500c5ba0e4c8570b9112fd7db27a
thaontp106 <thaonguyen040694@gmail.com>
2021-09-20 20:29:30 +0700

    Buổi 13 - Exception

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,16 +3,41 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws CheckException {
         Scanner scanner = new Scanner(System.in);
+        double w, h;
         try {
             System.out.print("Nhập cân nặng(kg): ");
-            double weight = scanner.nextDouble();
-            if (weight <= 0) throw new CheckException("Cân nặng không hợp lệ");
+            String weight = scanner.nextLine();
+            if (isDouble(weight)) {
+                w = Double.parseDouble(weight);
+            } else {
+                throw new NumberFormatException("Cân nặng nhập vào không đúng định dạng");
+            }
+            if (w <= 0) {
+                throw new CheckException("Cân nặng không hợp lệ");
+            }
             System.out.print("Nhập chiều cao(m): ");
-            double height = scanner.nextDouble();
-            if (height <= 0) throw new CheckException("Chiều cao không hợp lệ");
-            BMI.calculateBMI(weight, height);
-        } catch (Exception e) {
-            e.printStackTrace();
+            String height = scanner.nextLine();
+            if (isDouble(height)) {
+                h = Double.parseDouble(height);
+            } else {
+                throw new NumberFormatException("Chiều cao nhập vào không đúng định dạng");
+            }
+            if (h <= 0) {
+                throw new CheckException("Chiều cao không hợp lệ");
+            }
+            BMI.calculateBMI(w, h);
+        } catch(Exception e){
+                e.printStackTrace();
+            }
         }
+    private static boolean isDouble(String x) { //check số kiểu double
+        if (x == null)
+            return false;
+        try {
+            double a = Double.parseDouble(x);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
